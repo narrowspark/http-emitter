96922e3456da63fd0f2e57a99f23aa6f554f2d53
Daniel Bannert <d.bannert@anolilab.de>
2017-04-04 19:22:31 +0200

    Changed some line in SapiEmitter | removed SapiStreamEmitter for now

--- src/SapiEmitter.php
+++ src/SapiEmitter.php
@@ -29,26 +29,32 @@ class SapiEmitter extends AbstractSapiEmitter
      */
     public function emit(ResponseInterface $response)
     {
-        if (headers_sent()) {
-            throw new RuntimeException('Unable to emit response: headers already sent.');
+        $file = $line = null;
+
+        if (headers_sent($file, $line)) {
+            throw new RuntimeException(sprintf(
+                'Unable to emit response: Headers already sent in file %s on line %s.',
+                $file,
+                $line
+            ));
         }
 
         $response = $this->injectContentLength($response);
 
-        // Emit the HTTP status line
         $this->emitStatusLine($response);
-        // Emit the HTTP headers
         $this->emitHeaders($response);
-        // Emit the body
+
+        // Command line output buffering is disabled in cli by default.
+        if (php_sapi_name() == 'cli' || php_sapi_name() == 'phpdbg') {
+            $this->collectGarbage();
+
+            Util::closeOutputBuffers($this->maxBufferLevel ?? ob_get_level(), true);
+        }
+
         $this->sendBody($response);
 
         if (function_exists('fastcgi_finish_request')) {
             fastcgi_finish_request();
-        } elseif (php_sapi_name() == 'cli' || php_sapi_name() == 'phpdbg') {
-            // Command line output buffering is disabled in cli by default.
-            $this->collectGarbage();
-
-            Util::closeOutputBuffers($this->maxBufferLevel ?? ob_get_level(), true);
         }
     }
 
