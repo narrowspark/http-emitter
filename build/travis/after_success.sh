8a0f2731def390d6e6ea4c9a2a0e21b56149dc23
Daniel Bannert <daniel.bannert@trnd.com>
2017-04-04 13:07:37 +0200

    travis fixes

---
+++ build/travis/after_success.sh
@@ -0,0 +1,2 @@
+# Run codecov
+bash <(curl -s https://codecov.io/bash) -t 30813bf4-416c-4aa0-ae12-849b9b8323aa
---
+++ build/travis/script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Create logs dir
+mkdir -p build/logs
+#!/bin/bash
+
+set +e
+bash -e <<TRY
+    if [[ "$PHPSTAN" = true ]]; then
+        ./vendor/bin/phpstan analyse -c phpstan.neon -l 5 src
+    fi
+
+    if [[ "$PHPUNIT" = true && "$SEND_COVERAGE" = true ]]; then
+        ./vendor/bin/phpunit -c phpunit.xml.dist --verbose --coverage-text="php://stdout" --coverage-clover=coverage.xml;
+    fi
+TRY
+if [ $? -ne 0 ]; then
+  exit 1
+fi
