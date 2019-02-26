bfb2cc95585f1e570ac279b36195723ac958dd39
Rasmus Schultz <rasmus@mindplay.dk>
2019-02-26 14:24:34 +0100

    opinionated: drop the SAPI emitter that doesn't support streaming; we don't need two versions of the SAPI emitter - we need one SAPI emitter that works well for all scenarios

--- src/AbstractSapiEmitter.php
+++ src/AbstractSapiEmitter.php
@@ -8,25 +8,6 @@ use RuntimeException;
 abstract class AbstractSapiEmitter
 {
     /**
-     * Emit a response.
-     *
-     * Emits a response, including status line, headers, and the message body,
-     * according to the environment.
-     *
-     * Implementations of this method may be written in such a way as to have
-     * side effects, such as usage of header() or pushing output to the
-     * output buffer.
-     *
-     * Implementations MAY raise exceptions if they are unable to emit the
-     * response; e.g., if headers have already been sent.
-     *
-     * @param \Psr\Http\Message\ResponseInterface $response
-     *
-     * @return void
-     */
-    abstract public function emit(ResponseInterface $response): void;
-
-    /**
      * Assert either that no headers been sent or the output buffer contains no content.
      *
      * @throws \RuntimeException
