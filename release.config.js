6372514a423f224d26cdcc94c63db787c4c3ad15
Daniel Bannert <prisis@users.noreply.github.com>
2021-03-12 11:00:18 +0100

    chore: added missing allowed branches to semantic

--- release.config.js
+++ release.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    "branches": [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "main",
+        "next",
+        "next-major",
+        {
+            "name": "beta",
+            "prerelease": true
+        },
+        {
+            "name": "alpha",
+            "prerelease": true
+        }
+    ],
     "dryRun": false,
     "plugins": [
         [
